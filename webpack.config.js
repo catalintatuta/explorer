246c0657ca744bb2216591284e2f0d561f2e8694
Ionut Catalin Tatuta <catalin.tatuta@softbinator.com>
2024-03-25 22:03:07 +0100

    Update title

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,6 @@ module.exports = {
     }
   },
   plugins: [
-    new HtmlWebpackPlugin({'title': 'Three.js FPS Demo | Venolabs', template: './src/index.html'})
+    new HtmlWebpackPlugin({'title': 'Echoes Of Bytes', template: './src/index.html'})
   ]
 }
